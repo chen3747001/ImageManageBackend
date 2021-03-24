5922d491551942fc7985e29d8c186246a21385ab
ChenYuHeng <1006102565@qq.com>
2021-03-24 09:38:09 +0800

    上传初始化springboot项目

---
+++ src/main/java/com/chen/iamgemanage/IamgemanageApplication.java
@@ -0,0 +1,13 @@
+package com.chen.iamgemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamgemanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamgemanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chen/iamgemanage/IamgemanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.iamgemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamgemanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
